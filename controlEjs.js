5a2547c1a8105eeba630d48fe8c9be15511db4ed
Daniel García Pérez <95293092+Yurzian@users.noreply.github.com>
2022-11-11 10:58:06 +0100

    Add files via upload

--- controlEjs.js
+++ controlEjs.js
@@ -9,11 +9,18 @@ function swap(arr,v1,v2)
 
 function ordena(arr, numerico)
 {
-    let resul=arr;
     let menor=0;
-    if (numerico) //Si usamos una ordenacion numerica
+    let resul=arr;
+    
+    if (!numerico)
     {
-        for (let i=0; i<arr.length; i++)
+        for (let valor of resul)
+        {
+            valor = valor.toString();
+        }
+    }
+    
+    for (let i=0; i<arr.length; i++)
         {
             menor=i;
 
@@ -25,32 +32,108 @@ function ordena(arr, numerico)
                 }
             }
 
-            swap(arr,i,menor);//Creada por mi arriba
+            swap(arr,i,menor);//Creada por mi, arriba
         }
 
         return resul;
-    }
-    else //Si usamos una ordenacion alfabetica
-    {
-        for (let i=0; i<arr.length; i++)
-        {
-            menor=i;
-
-            for (let j=i;j<arr.length;j++)
-            {
-                if (String.resul[j] < String.resul[menor])
-                {
-                    menor = j;
-                }
-            }
-
-            swap(arr,i,menor);//Creada por mi arriba
-        }
-
-        return resul;
-    }
 }
+console.log("1.-");
 
 console.log (ordena([3, 2, 30, 4], true));  // devuelve un array nuevo ordenado numéricamente [2, 3, 4, 30] 
 
 console.log (ordena([3, 2, 30, 4], false));  // devuelve un array nuevo ordenado alfabéticamente [2, 3, 30, 4]
+
+////////////////////////////////////////////////////////////////////
+
+//2
+
+function tajada (arr, inicio, fin)
+{
+    let resul=[];
+
+    for (let i=inicio;i < fin; i++)
+    {
+        resul.push(arr[i]);
+    }
+
+    return resul;
+}
+
+console.log("\n2.-");
+
+console.log (tajada([3, 2, 30, 4], 1, 3)); // Devuelve un array nuevo [2, 30];
+
+///////////////////////////////////////////////////////////////////
+
+//3
+
+function romboHueco(num)
+{
+    //PARTE DE ARRIBA Y MEDIO
+    let espacios=num-1;
+    let asteriscos=1;
+    let string;
+    let rombo="";
+
+    for (let i=1;i<=num;i++)
+    {
+        string="";
+
+        for (let j=1;j<=espacios;j++)
+        {
+            string+=" ";
+        }
+
+        for (let j=1;j<=asteriscos;j++)
+        {
+            if (j==1 || j==asteriscos)
+            {
+                string+="*";
+            }
+            else
+            {
+                string+=" ";
+            }
+        }
+
+        asteriscos+=2;
+        espacios-=1;
+
+        rombo+= string+"\n";
+
+    }
+
+    //PARTE DE ABAJO
+    espacios=1;
+    asteriscos=(num*2)-3;
+
+    for (let i=1;i<=num-1;i++)
+    {
+        string="";
+        for (let j=1;j<=espacios;j++)
+        {
+            string+=" ";
+        }
+
+        for (let j=1;j<=asteriscos;j++)
+        {
+            if (j==1 || j==asteriscos)
+            {
+                string+="*";
+            }
+            else
+            {
+                string+=" ";
+            }
+        }
+
+        asteriscos-=2;
+        espacios+=1;
+        rombo+=string + "\n";
+    }
+    console.log(rombo);
+}
+
+console.log("\n3.-");
+
+romboHueco(4);
